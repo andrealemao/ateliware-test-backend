57398547af46577ed2f4812639a3066d4a7ca348
André Luiz Nunes Goulart <andreluizalemao@gmail.com>
2021-03-28 21:37:45 -0300

    add sql heroku

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('ateliwaredevteste', 'root', 'root', {
-    host: 'localhost',
+// const sequelize = new Sequelize('ateliwaredevteste', 'root', 'root', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// })
+const sequelize = new Sequelize('mysql-ateliware', 'b13d423df69031', '4372da29', {
+    host: 'us-cdbr-east-03.cleardb.com',
+    // port: 3306,
     dialect: 'mysql'
 })
 
